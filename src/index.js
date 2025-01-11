f5ab10f01be541b4e0ce94bb32e9a2e783dad000
scojr <180873402+scojr@users.noreply.github.com>
2025-01-11 17:29:59 -0600

    Add basic project functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import { greeting } from "./greeting.js";
+import "./style.css";
 
-console.log(greeting);
+const projects = [];
+
+export function getProjects() {
+  return projects;
+}
+
+export function insertProject(input) {
+  projects.push(input);
+}
+
+class Project {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  tasks = [];
+
+  get tasks() {
+    return tasks;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+}
+
+function newProject(name, description) {
+  insertProject(new Project(name, description));
+}
+
+newProject("Chores", "Daily chores to complete.");
+
+getProjects()[0].addTask("clean room");
+
+console.log(getProjects());
