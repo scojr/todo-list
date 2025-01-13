14aff33b60ff1dab9994f969331ef65132911c8c
scojr <180873402+scojr@users.noreply.github.com>
2025-01-13 17:27:31 -0600

    Add basic task functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import { greeting } from "./greeting.js";
+const tasks = [];
 
-console.log(greeting);
+class Task {
+  constructor(objective, details, deadline) {
+    this.title = objective;
+    this.details = details;
+    this.date = deadline;
+  }
+}
+
+function listTasks() {
+  return tasks;
+}
+
+function newTask(objective, details, deadline) {
+  const task = new Task(objective, details, deadline);
+  tasks.push(task);
+}
+
+newTask("clean room", "clean up my roomn", "tonight");
+console.log(listTasks());
