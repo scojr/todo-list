9771aaf3d18cd1aaec884fb9041eb7c3ca730865
scojr <180873402+scojr@users.noreply.github.com>
2025-01-15 17:05:26 -0600

    Introduce folders

--- src/display-handler.js
+++ src/display-handler.js
@@ -1,6 +1,7 @@
-import { getTasks, newTask } from "./tasks-handler";
+import { getFolders, newFolder, getTasks, newTask } from "./tasks-handler.js";
 
 export function refreshList() {
+
   const taskList = document.querySelector(".task-list");
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
@@ -10,6 +11,16 @@ export function refreshList() {
     taskItem.textContent = task.title;
     taskList.appendChild(taskItem);
   }
+
+  const folderList = document.querySelector(".folder-list");
+  while (folderList.firstChild) {
+    folderList.removeChild(folderList.firstChild);
+  }
+  for (const folder of getFolders()) {
+    const folderItem = document.createElement("li");
+    folderItem.textContent = folder.title;
+    folderList.appendChild(folderItem);
+  }
 }
 
 const addTaskButton = document.querySelector(".add-task-button");
@@ -18,6 +29,16 @@ addTaskButton.addEventListener("click", () => {
   const taskPrompt = prompt("Add new task");
   if (taskPrompt) {
     newTask(taskPrompt);
+    refreshList()
+  }
+});
+
+const addFolderButton = document.querySelector(".add-folder-button");
+
+addFolderButton.addEventListener("click", () => {
+  const folderPrompt = prompt("Add new folder");
+  if (folderPrompt) {
+    newFolder(folderPrompt);
     refreshList()
   }
 });
--- src/tasks-handler.js
+++ src/tasks-handler.js
@@ -1,10 +1,25 @@
 const tasks = [];
+const folders = [];
 
 class Task {
   constructor(objective, details, deadline) {
     this.title = objective;
     this.details = details;
-    this.date = deadline;
+    this.deadline = deadline;
+  }
+}
+
+class Folder {
+  constructor(name, details) {
+    this.title = name;
+    this.details = details;
+  }
+  tasks = [];
+  get tasks() {
+    return tasks;
+  }
+  set insert(task) {
+    this.tasks.push(task);
   }
 }
 
@@ -13,8 +28,17 @@ export function newTask(objective, details, deadline) {
   tasks.push(task);
 }
 
+export function newFolder(name, details) {
+  const folder = new Folder(name, details);
+  folders.push(folder);
+}
+
 export function getTasks() {
   return tasks;
+}
+
+export function getFolders() {
+  return folders;
 }
 
 newTask("clean room", "clean up my roomn", "tonight");
