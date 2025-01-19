36a5b95f1d36128d4f4e0c54ead8b7651719c264
scojr <180873402+scojr@users.noreply.github.com>
2025-01-18 18:28:18 -0600

    Improve layout and formatting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
-import { refreshList } from "./display-handler";
+import { refreshSidebar, displayDate } from "./sidebar-controller";
 
-refreshList();
+refreshSidebar();
+displayDate();
